458db8bffa174474b89b33c466cd37bd907ea613
Sheep Extraordinaire <76109159+rosehairedsheep@users.noreply.github.com>
2026-03-28 15:30:28 +0200

    basic setup of slices and player

---
+++ scenes/levels/level_slice.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+const DEFAULT_SCROLL_SPEED = 40.0
+var screen_height = 540
+
+@export var movable_space: Node2D
+var scroll_tween: Tween = null
+
+var lock_enabled: bool = false
+
+func toggle_lock(new_enabled: bool) -> void:
+	if (lock_enabled && !new_enabled):
+		lock_enabled = false ## TODO: add animations here
+		return
+	if (!lock_enabled && new_enabled):
+		lock_enabled = true ## TODO: add animations here
+		if scroll_tween: scroll_tween.kill()
+		return
+
+func initiate_scroll(speed: float) -> void:
+	if lock_enabled: return
+
+	movable_space.position.y = fmod(movable_space.position.y + speed, screen_height) ## FIXME: edit this
+	if movable_space.position.y > 0: movable_space.position.y -= screen_height ## this wraps to top
+
+func _on_slice_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_released("scroll_up"):
+		initiate_scroll(DEFAULT_SCROLL_SPEED)
+		return
+	if event.is_action_released("scroll_down"):
+		initiate_scroll(-DEFAULT_SCROLL_SPEED)
+		return
+
+func _on_slice_area_body_entered(body: Node2D) -> void:
+	if (body.is_in_group("player")): toggle_lock(true)
+
+func _on_slice_area_body_exited(body: Node2D) -> void:
+	if (body.is_in_group("player")): toggle_lock(false)
---
+++ scenes/player/player_character.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const RUN_SPEED = 100.0
+const JUMP_STRENGTH = 200.0
+const GRAVITY_STRENGTH = 120.0
+
+func _physics_process(delta: float) -> void:
+	velocity.x += (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * delta * RUN_SPEED
+
+	if Input.is_action_just_pressed("jump") && is_on_floor() : velocity.y -= JUMP_STRENGTH
+	velocity.y += GRAVITY_STRENGTH * delta
+
+	move_and_slide()
